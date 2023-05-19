5373ec848916bacb5ac644776cef1ef118fadc01
Ugbodaga Augustine <83676555+Austine4real@users.noreply.github.com>
2023-05-19 21:01:37 +0100

    first commit

---
+++ MYAPP-TODO/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibraryToDo;
+using System;
+
+namespace MYAPP_TODO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppEntry.MyAppEntry();
+        }
+    }
+}
